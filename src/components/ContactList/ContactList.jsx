84707874666703591951ae37417e2bdc2a36e405
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-05-23 10:55:07 +0300

    fix2

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,10 @@
 import { Contact } from 'components/Contact/Contact';
 import { List, Item } from './ContactList.styled';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 
 export const ContactList = () => {
-  let contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-  if (filter.length) {
-    contacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
+  const contacts = useSelector(selectFilteredContacts);
   return (
     <List>
       {contacts.map(({ name, number, id }) => (
